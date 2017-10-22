30df8a40b4a2bd0d855009f16dfed468e8e77870
sidereus <francesco.serafin.3@gmail.com>
2017-10-22 13:59:23 -0600

    Added variograms

--- src/functions/functions.R
+++ src/functions/functions.R
@@ -6,7 +6,13 @@ inputDataProcessing <- function(path) {
     inputData$dataora <- inputData$posix
     inputData$posix <- NULL
 
+    for (i in 1:length(colnames(inputData))) {
+        if (colnames(inputData)[i] == 90290)
+            index <- i
+    }
+
     row.names(inputData) <- inputData$dataora
+    inputData <- inputData[,-index]
 
     return(inputData)
 }
@@ -21,8 +27,8 @@ inputCoordinatesProcessing <- function(path) {
 
 dataCoordProcessing <- function(data, coordinate) {
 
-    df <- rep(list(),12)
-    coord <- rep(list(),12)
+    df <- rep(list(),length(data[1,]))
+    coord <- rep(list(),length(data[1,]))
 
     for(i in 1:length(data[1,])){
         tmp <- data.frame(array(0, dim=c(length(data[i,])-1,1)))
@@ -125,32 +131,102 @@ indexToMonth <- function(index) {
     return(month)
 }
 
-computeCrossValidation <- function(krigingType, variogramType, type, prec.fit, date, plotPath, nstations, coord) {
+computeCrossValidation <- function(krigingType, variogramType, type, prec.fit, date, plotPath, nstations, coord,
+                                   data_lin, data_exp, data_sph, data_gau,
+                                   data_ked_lin, data_ked_exp, data_ked_sph, data_ked_gau, data_bes, data_ked_bes) {
 
     for(i in 1:length(krigingType)) {
         kriging <- krigingType[i]
         if (kriging=="Ordinary") {
             prec.cv <- krige.cv(media ~ 1, loc=df_media, model=prec.fit)
             prec.kriged <- krige(media ~ 1, loc=df_media, demToGrid,model=prec.fit)
+
+            tmp <- raster(prec.kriged)
+            if (variogramType=="Lin") {
+                data_lin[timestep,1] <- as.character(date)
+                for (j in 1:length(saveVal$index)) {
+                    index <- saveVal$index[j]
+                    data_lin[timestep,j+1] <- as.numeric(tmp[index])
+                }
+            } else if (variogramType=="Gau") {
+                data_gau[timestep,1] <- as.character(date)
+                for (j in 1:length(saveVal$index)) {
+                    index <- saveVal$index[j]
+                    data_gau[timestep,j+1] <- as.numeric(tmp[index])
+                }
+            } else if (variogramType=="Sph") {
+                data_sph[timestep,1] <- as.character(date)
+                for (j in 1:length(saveVal$index)) {
+                    index <- saveVal$index[j]
+                    data_sph[timestep,j+1] <- as.numeric(tmp[index])
+                }
+            } else if (variogramType=="Exp") {
+                data_exp[timestep,1] <- as.character(date)
+                for (j in 1:length(saveVal$index)) {
+                    index <- saveVal$index[j]
+                    data_exp[timestep,j+1] <- as.numeric(tmp[index])
+                }
+            } else if (variogramType=="Bes") {
+                data_bes[timestep,1] <- as.character(date)
+                for (j in 1:length(saveVal$index)) {
+                    index <- saveVal$index[j]
+                    data_bes[timestep,j+1] <- as.numeric(tmp[index])
+                }
+            }
+
             if (nstations > 0) {
                 ## prec.kriged_loc <- krige(media ~ 1, loc=df_media,__missing_xyz__,model=prec.fit, nmax=10)
             }
         } else if (kriging=="KED") {
             prec.cv <- krige.cv(media ~ quota, loc=df_media, model=prec.fit)
             prec.kriged <- krige(media ~ quota, loc=df_media, demToGrid,model=prec.fit)
+
+            tmp <- raster(prec.kriged)
+            if (variogramType=="Lin") {
+                data_ked_lin[timestep,1] <- as.character(date)
+                for (j in 1:length(saveVal$index)) {
+                    index <- saveVal$index[j]
+                    data_ked_lin[timestep,j+1] <- as.numeric(tmp[index])
+                }
+            } else if (variogramType=="Gau") {
+                data_ked_gau[timestep,1] <- as.character(date)
+                for (j in 1:length(saveVal$index)) {
+                    index <- saveVal$index[j]
+                    data_ked_gau[timestep,j+1] <- as.numeric(tmp[index])
+                }
+            } else if (variogramType=="Sph") {
+                data_ked_sph[timestep,1] <- as.character(date)
+                for (j in 1:length(saveVal$index)) {
+                    index <- saveVal$index[j]
+                    data_ked_sph[timestep,j+1] <- as.numeric(tmp[index])
+                }
+            } else if (variogramType=="Exp") {
+                data_ked_exp[timestep,1] <- as.character(date)
+                for (j in 1:length(saveVal$index)) {
+                    index <- saveVal$index[j]
+                    data_ked_exp[timestep,j+1] <- as.numeric(tmp[index])
+                }
+            } else if (variogramType=="Bes") {
+                data_ked_bes[timestep,1] <- as.character(date)
+                for (j in 1:length(saveVal$index)) {
+                    index <- saveVal$index[j]
+                    data_ked_bes[timestep,j+1] <- as.numeric(tmp[index])
+                }
+            }
+
             if (nstations > 0) {
                 ## prec.kriged_loc <- krige(media ~ elev, loc=df_media,__missing_xyz__,model=prec.fit, nmax=10)
             }
         }
-
+        
         pdf(paste(plotPath,"observed_predict_",type,"_",date,"_",variogramType,"_krig_", kriging,".pdf",sep=""))
         plot(prec.cv$observed,prec.cv$var1.pred,main=paste("observed_predict_",type,"_",date,"_",variogramType,"_krig_",kriging,sep=""))
         abline(0,1)
         dev.off()
 
-        #pdf(paste(plotPath,"krig_",kriging,"_variogram_",variogramType,".pdf",sep=""))
-        #spplot(prec.kriged,"var1.pred",contour=T,col.regions=rainbow(100,start=.5,end=.75),main="Kringing precipitazione - inverno 2004")
-        #dev.off()
+        pdf(paste(plotPath,"krig_",kriging,"_variogram_",variogramType,"_date_",date,".pdf",sep=""))
+        spplot(prec.kriged,"var1.pred",contour=T,col.regions=rainbow(100,start=.5,end=.75),main=paste("Kringing temperatura -",date))
+        dev.off()
         if (nstations > 0) {
             ## pdf(paste(plotPath,"krig_",kriging,"_variogram_",variogramType,"_localnstat",nstations,".pdf",sep=""))
             ## ssplot(prec.kriged,"var1.pred",contour=T,col.regions=rainbow(100,start=.5,end=.75),main="Kringing precipitazione - inverno 2004")
@@ -158,9 +234,26 @@ computeCrossValidation <- function(krigingType, variogramType, type, prec.fit, d
         }
     }
 
+    dataout <- rep(list(),10)
+
+    dataout[[1]] <- data_lin
+    dataout[[2]] <- data_exp
+    dataout[[3]] <- data_sph
+    dataout[[4]] <- data_gau
+    dataout[[5]] <- data_ked_lin
+    dataout[[6]] <- data_ked_exp
+    dataout[[7]] <- data_ked_sph
+    dataout[[8]] <- data_ked_gau
+    dataout[[9]] <- data_bes
+    dataout[[10]] <- data_ked_bes
+
+    return(dataout)
+
 }
 
-variogramFitting <- function(singleVariogram, krigingType, monthNum, date, type, dataPath, plotPath, nstations, coord) {
+variogramFitting <- function(singleVariogram, krigingType, monthNum, date, type, dataPath, plotPath, nstations, coord,
+                             data_lin, data_exp, data_sph, data_gau,
+                             data_ked_lin, data_ked_exp, data_ked_sph, data_ked_gau, data_bes, data_ked_bes) {
 
     monthName <- indexToMonth(monthNum)
 
@@ -190,7 +283,34 @@ variogramFitting <- function(singleVariogram, krigingType, monthNum, date, type,
                    sub=paste("sill ",prec.fit$psill[2]," - range ",prec.fit$range[2])))
         dev.off()
 
-        computeCrossValidation(krigingType, variogramType, type, prec.fit, date, plotPath, nstations, coord)
+        outdata <- computeCrossValidation(krigingType, variogramType, type, prec.fit, date, plotPath, nstations, coord,
+                                          data_lin, data_exp, data_sph, data_gau,
+                                          data_ked_lin, data_ked_exp, data_ked_sph, data_ked_gau, data_bes, data_ked_bes)
+        data_lin <- outdata[[1]]
+        data_exp <- outdata[[2]]
+        data_sph <- outdata[[3]]
+        data_gau <- outdata[[4]]
+        data_ked_lin <- outdata[[5]]
+        data_ked_exp <- outdata[[6]]
+        data_ked_sph <- outdata[[7]]
+        data_ked_gau <- outdata[[8]]
+        data_bes <- outdata[[9]]
+        data_ked_bes <- outdata[[10]]
     }
+
+    dataout <- rep(list(),10)
+
+    dataout[[1]] <- data_lin
+    dataout[[2]] <- data_exp
+    dataout[[3]] <- data_sph
+    dataout[[4]] <- data_gau
+    dataout[[5]] <- data_ked_lin
+    dataout[[6]] <- data_ked_exp
+    dataout[[7]] <- data_ked_sph
+    dataout[[8]] <- data_ked_gau
+    dataout[[9]] <- data_bes
+    dataout[[10]] <- data_ked_bes
+
+    return(dataout)
 
 }
--- src/main.R
+++ src/main.R
@@ -6,6 +6,7 @@ library(gstat)
 library(sp)
 library(rgdal)
 library(tools)
+library(raster)
 library(lubridate)
 library(parallel)
 library(foreach)
@@ -29,10 +30,32 @@ fileNames <- list.files(paste(dataPath), pattern="*.csv")
 
 nstations <- 10 # for local kriging
 
+dem <- raster(paste(coordPath,"DEM.xyz",sep=""),sep=" ")
 demToGrid <- read.table(paste(coordPath,"DEM.xyz",sep=""),sep=" ")
 colnames(demToGrid) <- c("x","y","quota")
 gridded(demToGrid) <- ~x+y
 proj4string(demToGrid) <- CRS("+init=epsg:32632 +proj=utm +zone=32N ellps=WGS84")
+
+stationsIndex <- function() {
+
+    coordinate$name <- row.names(coordinate)
+    coordinates(coordinate) <- ~long+lat
+    tmpCoord <- latlong2utm32n(coordinate)
+    tmpCoord.df <- data.frame(index=coordinates(tmpCoord)[,1],
+                              quota=tmpCoord$quota,
+                              name=tmpCoord$name)
+
+    for (i in 1:length(tmpCoord.df[,1])) {
+        point = matrix(nrow=1,ncol=2)
+        point[1,1] = coordinates(tmpCoord)[i,1]
+        point[1,2] = coordinates(tmpCoord)[i,2]
+
+        tmpCoord.df[i,1] = cellFromXY(dem, point)
+    }
+
+    return(tmpCoord.df)
+
+}
 
 for (nFile in 1:length(files)) {
 
@@ -40,25 +63,45 @@ for (nFile in 1:length(files)) {
     type <- file_path_sans_ext(fileNames[nFile])
     print(type)
     data <- inputDataProcessing(dataFile)
+
+    outdata_lin <- matrix(nrow=length(data[,1]),ncol=length(data[1,]))
+    outdata_lin <- data.frame(outdata_lin)
+    colnames(outdata_lin) <- colnames(data)
+
+    outdata_exp <- matrix(nrow=length(data[,1]),ncol=length(data[1,]))
+    outdata_exp <- data.frame(outdata_exp)
+    colnames(outdata_exp) <- colnames(data)
+
+    outdata_sph <- matrix(nrow=length(data[,1]),ncol=length(data[1,]))
+    outdata_sph <- data.frame(outdata_sph)
+    colnames(outdata_sph) <- colnames(data)
+
+    outdata_gau <- matrix(nrow=length(data[,1]),ncol=length(data[1,]))
+    outdata_gau <- data.frame(outdata_gau)
+    colnames(outdata_gau) <- colnames(data)
+
+    outdata_bes <- matrix(nrow=length(data[,1]),ncol=length(data[1,]))
+    outdata_bes <- data.frame(outdata_bes)
+    colnames(outdata_bes) <- colnames(data)
+
+    outdata_ked_lin <- outdata_lin
+    outdata_ked_exp <- outdata_exp
+    outdata_ked_sph <- outdata_sph
+    outdata_ked_gau <- outdata_gau
+    outdata_ked_bes <- outdata_bes
+
     coordinate <- inputCoordinatesProcessing(coordPath)
 
     list[totaldf,totalcoord] <- dataCoordProcessing(data, coordinate)
 
-    oldMonthNum <- 0
+    saveVal <- stationsIndex()
 
-    foreach (i=1:length(data[,1]), .inorder=FALSE, .packages=c("gstat","lubridate","tools","rgdal")) %dopar% {
-    #for (i in 1:length(data[,1])) {
+    #foreach (i=1:length(data[,1]), .inorder=FALSE, .packages=c("gstat","lubridate","tools","rgdal","raster")) %dopar% {
+    for (i in 1:length(data[,1])) {
+        timestep <- i
         date <- data[i,1]
         monthNum <- month(as.POSIXlt(date))
         print(date)
-
-        ## if (monthNum > oldMonthNum) {
-        ##     head <- colnames(data)
-        ##     write(head, file=paste(outDataPath,"monthNum",sep=""),sep=",")
-        ##     oldMonthNum <- oldMonthNum + 1
-        ## } else {
-        ##     write(val, file=paste(outDataPath,monthNum,".csv"))
-        ## }
 
         df_media <- totaldf[[i]]
         coord <- totalcoord[[i]]
@@ -70,10 +113,34 @@ for (nFile in 1:length(files)) {
         omnidirectSperimentalVariogram(date, plotPath, type)
         singleVariogram <- c("Lin","Sph","Exp","Gau")
         krigingType <- c("Ordinary", "KED")
-        variogramFitting(singleVariogram, krigingType,
-                         monthNum, date, type, dataPath,
-                         plotPath, nstations, coord)
+        outdata <- variogramFitting(singleVariogram, krigingType,
+                                    monthNum, date, type, dataPath,
+                                    plotPath, nstations, coord,
+                                    outdata_lin, outdata_exp, outdata_sph, outdata_gau,
+                                    outdata_ked_lin, outdata_ked_exp, outdata_ked_sph, outdata_ked_gau, outdata_bes, outdata_ked_bes)
+        outdata_lin <- outdata[[1]]
+        outdata_exp <- outdata[[2]]
+        outdata_sph <- outdata[[3]]
+        outdata_gau <- outdata[[4]]
+        outdata_ked_lin <- outdata[[5]]
+        outdata_ked_exp <- outdata[[6]]
+        outdata_ked_sph <- outdata[[7]]
+        outdata_ked_gau <- outdata[[8]]
+        outdata_bes <- outdata[[9]]
+        outdata_ked_bes <- outdata[[10]]
 
     }
+
+    write.csv(outdata_lin, paste(outDataPath,"test",type,"lin.csv",sep=""), sep=",",row.names=FALSE)
+    write.csv(outdata_gau, paste(outDataPath,"test",type,"gau.csv",sep=""), sep=",",row.names=FALSE)
+    write.csv(outdata_sph, paste(outDataPath,"test",type,"sph.csv",sep=""), sep=",",row.names=FALSE)
+    write.csv(outdata_exp, paste(outDataPath,"test",type,"exp.csv",sep=""), sep=",",row.names=FALSE)
+    write.csv(outdata_bes, paste(outDataPath,"test",type,"bes.csv",sep=""), sep=",",row.names=FALSE)
+
+    write.csv(outdata_ked_lin, paste(outDataPath,"test",type,"ked_lin.csv",sep=""), sep=",",row.names=FALSE)
+    write.csv(outdata_ked_gau, paste(outDataPath,"test",type,"ked_gau.csv",sep=""), sep=",",row.names=FALSE)
+    write.csv(outdata_ked_sph, paste(outDataPath,"test",type,"ked_sph.csv",sep=""), sep=",",row.names=FALSE)
+    write.csv(outdata_ked_exp, paste(outDataPath,"test",type,"ked_exp.csv",sep=""), sep=",",row.names=FALSE)
+    write.csv(outdata_ked_bes, paste(outDataPath,"test",type,"bes_exp.csv",sep=""), sep=",",row.names=FALSE)
 
 }
